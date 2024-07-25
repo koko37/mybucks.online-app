ba41a65ead5494e362051640bb41de810d699634
koko37 <engel0088@gmail.com>
2024-07-25 21:30:19 +0800

    update vite conf for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mybucks.online/',
   resolve: {
     alias: {
       "@mybucks": path.resolve(__dirname, "./src"),
